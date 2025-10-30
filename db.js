9cd4a77460258c17e021428d419db821b96f9cda
gulnaz <gulnazhakimova70@gmail.com>
2025-10-30 10:49:06 +0300

    removed selectel, bd

---
+++ db.js
@@ -0,0 +1,7 @@
+// const { Sequelize } = require('sequelize');
+
+// module.exports = new Sequelize('telega', 'root', 'root', {
+//   host: 'master.f34bf178-9e64-48c1-bbba-18a6c4b27677.c.dbaas.selcloud.ru',
+//   port: '5432',
+//   dialect: 'postgres'
+// });
--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-//import TelegramApi from 'node-telegram-bot-api';
 const TelegramApi = require('node-telegram-bot-api');
 const { gameOptions, againOptions } = require('./options');
+// const sequelize = require('./db');
+// const UserModel = require('./models');
 
 const token = '8288459743:AAHbu7oB5UyXyuPnVn1UseARqvV3RRu51EE';
 
 const bot = new TelegramApi(token, { polling: true });
-
-bot.setMyCommands([
-  { command: '/start', description: 'Начальное приветствие' },
-  { command: '/info', description: 'Описание' },
-  { command: '/game', description: 'Игра угадай цифру' }
-]);
 
 const chats = {};
 
@@ -21,52 +16,75 @@ const startGame = async (chatId) => {
   await bot.sendMessage(chatId, 'Отгадывай!', gameOptions);
 };
 
-const start = () => {
-  bot.on('message', async (msq) => {
-    const text = msq.text;
-    const chatId = msq.chat.id;
-    //bot.sendMessage(chatId, `Ты написал мне ${text}`);
+const start = async () => {
+  // try {
+  //   await sequelize.authenticate();
+  //   await sequelize.sync();
+  // } catch {
+  //   console.log('Подключение к бд сломалось', e);
+  // }
 
-    if (text === '/start') {
-      await bot.sendSticker(
-        chatId,
-        'https://tlgrm.ru/_/stickers/b0d/85f/b0d85fbf-de1b-4aaf-836c-1cddaa16e002/1.webp'
-      );
-      return bot.sendMessage(chatId, 'Приветик!');
+  bot.setMyCommands([
+    { command: '/start', description: 'Начальное приветствие' },
+    { command: '/info', description: 'Описание' },
+    { command: '/game', description: 'Игра угадай цифру' }
+  ]);
+
+  bot.on('message', async (msg) => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+
+    try {
+      if (text === '/start') {
+        // await UserModel.create(chatId);
+        await bot.sendSticker(
+          chatId,
+          'https://tlgrm.ru/_/stickers/b0d/85f/b0d85fbf-de1b-4aaf-836c-1cddaa16e002/1.webp'
+        );
+        return bot.sendMessage(chatId, 'Приветик!');
+      }
+
+      if (text === '/info') {
+        // const user = await UserModel.findOne({ chatId });
+        return bot.sendMessage(
+          chatId,
+          `Тебя зовут ${msg.from.first_name}`
+          // `Тебя зовут ${msg.from.first_name}, в игре у тебя правильных ответов ${user.right}, неправильных ${user.wrong}`
+        );
+      }
+
+      if (text === '/game') {
+        return startGame(chatId);
+      }
+
+      return bot.sendMessage(chatId, 'Я тебя не понимаю');
+    } catch (e) {
+      return bot.sendMessage(chatId, 'Произошла какая то ошибочка!)');
     }
-
-    if (text === '/info') {
-      return bot.sendMessage(chatId, `Тебя зовут ${msq.from.first_name}`);
-    }
-
-    if (text === '/game') {
-      return startGame(chatId);
-    }
-
-    return bot.sendMessage(chatId, 'Ниче не понял');
   });
 
-  bot.on('callback_query', async (msq) => {
-    const data = msq.data;
-    const chatId = msq.message.chat.id;
+  bot.on('callback_query', async (msg) => {
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
 
     if (data === '/again') {
       return startGame(chatId);
     }
 
-    if (data === chats[chatId]) {
-      return await bot.sendMessage(
-        chatId,
-        `Поздравляю, ты отгадал цифру ${chats[chatId]}`,
-        againOptions
-      );
+    // const user = await UserModel.findOne({ chatId });
+
+    if (data == chats[chatId]) {
+      // user.right += 1;
+      await bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`, againOptions);
     } else {
-      return await bot.sendMessage(
+      // user.wrong += 1;
+      await bot.sendMessage(
         chatId,
         `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`,
         againOptions
       );
     }
+    await user.save();
   });
 };
 
